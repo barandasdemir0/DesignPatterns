1d14af5569a243399be945df9f4faf1e92c94161
Baran <barandasdemir.bd@gmail.com>
2025-08-01 15:37:00 +0300

    composite_desing_pattern

---
+++ CompositeDesingPattern/DesingPattern.Composite/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesingPattern.Composite.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=BARANPC\\SQLEXPRESS;Database=DesingPatternComposite;integrated security=True; TrustServerCertificate=True;");
+        }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+    }
+}
